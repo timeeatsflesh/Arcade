5f0096d5600dcf2b69190d5db7bddb75625b1185
timeeatsflesh <Robert@Roberts-MacBook.local>
2023-02-10 13:08:40 -0700

    current push

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-const squares = document.querySelectorAll('.board > div');
+const squares = document.querySelectorAll(".board > div");
 let currentPlayer = "X";
 
 for (let i = 0; i < squares.length; i++) {
@@ -11,9 +10,36 @@ for (let i = 0; i < squares.length; i++) {
       } else {
         currentPlayer = "X";
       }
-      document.getElementById("current-player").innerHTML = currentPlayer + " is up next.";
+      document.getElementById("current-player").innerHTML =
+        currentPlayer + " is up next.";
+      checkForWinner();
     }
   });
+}
+
+function checkForWinner() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      squares[a].textContent === squares[b].textContent &&
+      squares[b].textContent === squares[c].textContent &&
+      squares[a].textContent !== ""
+    ) {
+      document.getElementById("current-player").innerHTML =
+        squares[a].textContent + " is the winner!";
+    }
+  }
 }
 
 const resetButton = document.querySelector("#reset-button");
@@ -33,7 +59,6 @@ player2Input.addEventListener("input", function () {
   }
 });
 
-
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
 
@@ -41,7 +66,6 @@ const startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-  
   const randomNumber = Math.floor(Math.random() * 2);
   if (randomNumber === 0) {
     currentPlayer = player1;
@@ -49,6 +73,16 @@ function startGame() {
     currentPlayer = player2;
   }
 
-  
-  document.getElementById("current-player").innerHTML = currentPlayer.value + " goes first!";
+  document.getElementById("current-player").innerHTML =
+    currentPlayer.value + " goes first!";
+}
+
+function checkForDraw() {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i].textContent === "") {
+      return false;
+    }
+  }
+  document.getElementById("current-player").innerHTML = "It's a draw!";
+  return true;
 }
